326cdbfcfef671762fe516a94e3924c02c987169
Bryan J. R. Gislason <bryan-gislason@users.noreply.github.com>
2017-09-28 16:16:52 -0700

    Dropdown styles (#61)
    * update fabric dependency to 4.50.0
    
    * Avatar: Fix css override for persona markup change
    
    * Update Dropdown visual diffs from Fabric updates
    
    * Dropdown: add YamUI Down Icon as custom caretDown
    
    * update fabric dependency to 4.50.1
    
    * Fix the caretDown position since updating Fabric React
    
    * Dropdown: update styles to match design specifications
    
    * Update Dropdown visual diffs from style updates
    
    * Dropdown: update snapshot to handle onRenderCaretDown
    
    * dropdown: swap palette color with semantic $linkColor
    
    * dropdown: add focusedBorderColor css variable
    
    * Dropdown: use palette colors for dropdown options
    
    * Fixing style issues
    
    - Border radius for dropdown and callout
    - Hover state for dropdown
    - Cleaning callout borders, adjusting shadows
    
    * Dropdown: use semantic css variables for dropdownOption state colors
    
    * Update visual test references
    
    * Dropdown: use caretDown instead of down icon
    
    * update yarn.lock
    
    * update visual tests
    
    * update fabric to 5.1.0
    
    * update visual references for dropdown
    
    * Theme: fix visual errors by using new loadTheme. Auto is no longer an option for WebkitAntiAliasing. Fabric is by default antialiased
    
    * dropdown: use titleIsPlaceholder class to give the correct color for the placeholder text
    
    * fix theme test
    
    * Design adjustments
    
    - Fixing position of the carrot
    - Fixing change of text color on hover when option is not selected
    
    * update dropdown reference images
    
    * update visual references

--- src/util/fabric/theme.ts
+++ src/util/fabric/theme.ts
@@ -5,54 +5,53 @@
  * soon Fabric will have a loadTheme() utility to handle this.
  */
 
-const fontStyles = {
-  WebkitFontSmoothing: 'auto',
+import { loadTheme, IRawStyle } from 'office-ui-fabric-react/lib/Styling';
+
+const fontStyles : IRawStyle = {
   color: '#343A41',
 };
 
 // NOTE: Would be handy to have access to our CSS variable values in JS...
-(window as any).FabricConfig = {
-  theme: {
-    fonts: {
-      mini: fontStyles,
-      xSmall: {
-        ...fontStyles,
-        fontSize: '1.0rem',
-        lineHeight: '1.6rem',
-      },
-      small: {
-        ...fontStyles,
-        fontSize: '1.2rem',
-        lineHeight: '1.6rem',
-      },
-      smallPlus: {
-        ...fontStyles,
-        fontSize: '1.4rem',
-        lineHeight: '2.0rem',
-      },
-      medium: {
-        ...fontStyles,
-        fontSize: '1.5rem',
-        lineHeight: '2.0rem',
-      },
-      mediumPlus: fontStyles,
-      large: {
-        ...fontStyles,
-        fontSize: '1.8rem',
-        lineHeight: '2.4rem',
-      },
-      xLarge: {
-        ...fontStyles,
-        fontSize: '2.2rem',
-        lineHeight: '2.8rem',
-      },
-      xxLarge: {
-        ...fontStyles,
-        fontSize: '2.4rem',
-        lineHeight: '2.8rem',
-      },
-      superLarge: fontStyles,
-      mega: fontStyles,
+loadTheme({
+  fonts: {
+    tiny: fontStyles,
+    xSmall: {
+      ...fontStyles,
+      fontSize: '1.0rem',
+      lineHeight: '1.6rem',
     },
+    small: {
+      ...fontStyles,
+      fontSize: '1.2rem',
+      lineHeight: '1.6rem',
+    },
+    smallPlus: {
+      ...fontStyles,
+      fontSize: '1.4rem',
+      lineHeight: '2.0rem',
+    },
+    medium: {
+      ...fontStyles,
+      fontSize: '1.5rem',
+      lineHeight: '2.0rem',
+    },
+    mediumPlus: fontStyles,
+    large: {
+      ...fontStyles,
+      fontSize: '1.8rem',
+      lineHeight: '2.4rem',
+    },
+    xLarge: {
+      ...fontStyles,
+      fontSize: '2.2rem',
+      lineHeight: '2.8rem',
+    },
+    xxLarge: {
+      ...fontStyles,
+      fontSize: '2.4rem',
+      lineHeight: '2.8rem',
+    },
+    superLarge: fontStyles,
+    mega: fontStyles,
   },
-};
+});
