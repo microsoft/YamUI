3e94b46c2ac500fcb774643e48cb6de96b19aca6
Rich Tagger <me@richardtagger.com>
2017-10-19 20:04:45 -0700

    Add MessageBar component

---
+++ src/components/MessageBar/message-bar.test.tsx
@@ -0,0 +1,42 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+import * as React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+import MessageBar, { MessageBarType, MessageBarProps } from './index';
+import FakeLink from '../../components/FakeLink';
+
+describe('<FakeLink />', () => {
+  let component: ShallowWrapper<MessageBarProps, {}>;
+
+  describe('with default options', () => {
+    beforeEach(() => {
+      component = shallow(<MessageBar ariaLabel="label">content</MessageBar>);
+    });
+
+    it('matches its snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('with a type', () => {
+    beforeEach(() => {
+      component = shallow(
+        <MessageBar ariaLabel="label" type={MessageBarType.WARNING}>content</MessageBar>);
+    });
+
+    it('matches its snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('with actions', () => {
+    beforeEach(() => {
+      const link = <FakeLink>Link</FakeLink>;
+      component = shallow(
+        <MessageBar ariaLabel="label" actions={link}>content</MessageBar>);
+    });
+
+    it('matches its snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+});
