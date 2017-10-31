41e0bfe59c302771875d434d6a266577bbc41d2a
Daniel Perez Alvarez <unindented@gmail.com>
2017-10-30 18:41:01 -0700

    Disable animations on visual diffs

--- styleguide.config.visualdiff.js
+++ styleguide.config.visualdiff.js
@@ -1,6 +1,10 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+const path = require('path');
 const config = require('./styleguide.config');
 
 module.exports = Object.assign({}, config, {
   showSidebar: false,
+  require: config.require.concat([
+    path.resolve(__dirname, 'config/styleguide/assets/animations.css'),
+  ]),
 });
