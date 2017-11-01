a43385285ee949408a1d490076baeea8152b4ff7
Daniel Perez Alvarez <unindented@gmail.com>
2017-10-31 17:25:36 -0700

    Remove all traces of Storybook

--- config/styleguide-visual/config.js
+++ config/styleguide-visual/config.js
@@ -1,6 +1,8 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+const path = require('path');
+
 module.exports = {
-  url: 'http://localhost:5555',
+  url: `file://${path.resolve(__dirname, '../../build/docs/index.html')}`,
   dir: 'config/styleguide-visual/reference/',
   sandbox: false,
 };
