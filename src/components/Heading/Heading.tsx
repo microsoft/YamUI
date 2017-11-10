453fd953090b6916781e0d79e1aabc438a3acee2
swese44 <swese44@gmail.com>
2017-11-10 14:19:03 -0800

    comment cleanup

--- src/components/Heading/Heading.tsx
+++ src/components/Heading/Heading.tsx
@@ -13,14 +13,17 @@ export interface HeadingProps extends NestableBaseComponentProps {
    * The heading level to render, i.e. h1-h6
    */
   level: HeadingLevel;
-  /** An optional */
+
+  /** Allows a heading tag at a given level to render visually as a different level,
+   * or as plain inline text.
+   */
   size?: HeadingSize;
 }
 
 /**
- * A `Heading` component dynamically generates an h1-h6 tag depending on the given level. You can
- * also override its visual styling to match a different level with `size`. Set `size="none"` to
- * render the heading as plain unstyled text.
+ * A `Heading` component renders an h1-h6 tag depending on the given level. You can also
+ * override its visual styling to match a different level with `size`. Set `size="none"`
+ * to render the heading as unstyled inline text.
  */
 export default class Heading extends React.PureComponent<HeadingProps, {}> {
   render() {
