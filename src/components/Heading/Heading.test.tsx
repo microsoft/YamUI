84f7aaff12bce8fc80d72074950054d274cf6426
P.J. Swesey <swese44@gmail.com>
2017-11-15 15:27:22 -0800

    Headings h1-h6 (#111)
    * Heading component
    
    * visual diffs
    
    * remove redundant checklist item
    
    * comment cleanup
    
    * updated screenshots
    
    * visual diffs
    
    * simplify template, remove extra reference image

---
+++ src/components/Heading/Heading.test.tsx
@@ -0,0 +1,72 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+import * as React from 'react';
+import { shallow, ShallowWrapper } from 'enzyme';
+import Heading, { HeadingProps } from '.';
+
+
+describe('<Heading />', () => {
+  let component: ShallowWrapper<HeadingProps, {}>;
+
+  describe('with a level', () => {
+    beforeEach(() => {
+      component = shallow(<Heading level="1">test content</Heading>);
+    });
+
+    it('renders its given content', () => {
+      expect(component.text()).toEqual('test content');
+    });
+
+    it('renders the correct tag', () => {
+      expect(component.find('h1').length).toBe(1);
+    });
+
+    it('contains its base className', () => {
+      expect(component.hasClass('y-heading')).toBe(true);
+    });
+
+    it('matches its snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('with a size', () => {
+    beforeEach(() => {
+      component = shallow(<Heading level="2" size="3">test content</Heading>);
+    });
+
+    it('renders the correct size className', () => {
+      expect(component.hasClass('y-heading__size-3')).toBe(true);
+    });
+
+    it('still has its base className', () => {
+      expect(component.hasClass('y-heading')).toBe(true);
+    });
+
+    it('still renders the correct tag level', () => {
+      expect(component.find('h2').length).toBe(1);
+    });
+
+    it('matches its snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('with additional className', () => {
+    beforeEach(() => {
+      component = shallow(<Heading level="4" className="TEST_CLASSNAME">test content</Heading>);
+    });
+
+    it('includes that className', () => {
+      expect(component.hasClass('TEST_CLASSNAME')).toBe(true);
+    });
+
+    it('still includes its base className', () => {
+      expect(component.hasClass('y-heading')).toBe(true);
+    });
+
+    it('matches its snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+});
