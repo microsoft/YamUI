3443f850000c2cd25d1b32b86cb55bcf8a8ffee5
Bryan Gislason <brgisla@microsoft.com>
2017-11-20 17:33:04 -0800

    NavListItemNotification: vertical align center the badge and avatars to the content it is within

--- src/demos/NavListItem/NavListItemNotification.tsx
+++ src/demos/NavListItem/NavListItemNotification.tsx
@@ -20,22 +20,24 @@ export default class NavListItemNotification extends React.PureComponent<
 
     return (
       <div className="y-nav-list-item-notification">
-        <Fade showAfter={500} duration={500} hideAfter={6000} onAnimationEnd={onAnimationEnd}>
-          <FixedGridRow>
-            <FixedGridColumn>
-              <div className="y-nav-list-item-notification__avatars">
-                <Slide showAfter={1000} duration={500} hideAfter={4000}>
-                  <NavListItemAvatars avatars={avatars} />
-                </Slide>
-              </div>
-            </FixedGridColumn>
-            <FixedGridColumn fixed={true}>
-              <div className="y-nav-list-item-notification__badge">
-                <NavListItemBadge value={count} previousValue={previousCount} />
-              </div>
-            </FixedGridColumn>
-          </FixedGridRow>
-        </Fade>
+        <div className="y-nav-list-item-notification-wrapper">
+          <Fade showAfter={500} duration={500} hideAfter={6000} onAnimationEnd={onAnimationEnd}>
+            <FixedGridRow>
+              <FixedGridColumn>
+                <div className="y-nav-list-item-notification__avatars">
+                  <Slide showAfter={1000} duration={500} hideAfter={4000}>
+                    <NavListItemAvatars avatars={avatars} />
+                  </Slide>
+                </div>
+              </FixedGridColumn>
+              <FixedGridColumn fixed={true}>
+                <div className="y-nav-list-item-notification__badge">
+                  <NavListItemBadge value={count} previousValue={previousCount} />
+                </div>
+              </FixedGridColumn>
+            </FixedGridRow>
+          </Fade>
+        </div>
       </div>
     );
   }
