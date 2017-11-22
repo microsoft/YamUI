9cfb8fe9106f8f268ec5becb5c5112c40aaaeb3a
Daniel Perez Alvarez <unindented@gmail.com>
2017-11-22 13:03:28 -0800

    Do not use ES6 features in action, as it doesn't get transpiled

--- config/styleguide/context/action.js
+++ config/styleguide/context/action.js
@@ -1,5 +1,8 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
-module.exports = msg => (...args) => {
-  // eslint-disable-next-line no-console
-  console.log(msg, ...args);
+/* eslint-disable */
+module.exports = function(msg) {
+  return function() {
+    console.log(msg, arguments);
+  };
 };
+/* eslint-enable */
