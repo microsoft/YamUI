8f8d381b782fb7f996458d196210e691cf16405f
P.J. Swesey <swese44@gmail.com>
2017-12-05 17:21:49 -0800

    Remove secureOpener (#130)

--- src/util/secureOpener.ts
+++
@@ -1,9 +0,0 @@
-/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
-function secureOpen(url: string) {
-  const newWindow = window.open(url, '_blank');
-  if (newWindow) {
-    newWindow.opener = null;
-  }
-}
-
-export { secureOpen };
