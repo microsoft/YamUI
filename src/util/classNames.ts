016654f1a0cdff4070a7f54f64c5d4ca583e2527
swese44 <swese44@gmail.com>
2017-12-14 13:53:22 -0800

    strict equals per feedback

--- src/util/classNames.ts
+++ src/util/classNames.ts
@@ -2,7 +2,7 @@
 export type ClassName = string | undefined;
 
 export const join = (classNames: ClassName[]): string => {
-  if (!classNames.length) {
+  if (classNames.length === 0) {
     return '';
   }
 
