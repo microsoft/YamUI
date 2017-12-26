66c5618791b3ab8e3eb48e4dac843e1bb838e2af
Rich Tagger <me@richardtagger.com>
2017-12-26 12:16:51 +0000

    [SuggestionsList] props.target to support IPoint
    Pass through prop type support IPoint as Callout does

--- src/components/SuggestionsList/SuggestionsList.tsx
+++ src/components/SuggestionsList/SuggestionsList.tsx
@@ -8,12 +8,13 @@ import Text, { TextSize, TextColor } from '../Text';
 import Callout, { DirectionalHint } from '../Callout';
 import Block, { GutterSize } from '../Block';
 import './SuggestionsList.css';
+import { IPoint } from 'office-ui-fabric-react/lib/utilities/positioning';
 
 export interface SuggestionsListProps extends NestableBaseComponentProps {
   /**
    * The target that the Callout should try to position itself based on.
    */
-  target: HTMLElement;
+  target:  HTMLElement | IPoint;
   /**
    * The active search that produced the current state.
    */
