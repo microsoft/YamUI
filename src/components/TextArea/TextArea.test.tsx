e36c8b73e2a7331bfbc88814674128a8eeb60895
Chris Mohr <chrismohr@users.noreply.github.com>
2018-01-10 13:08:29 -0800

    Introduce TextArea (#172)
    * introduce TextArea
    
    * add reference images for TextArea
    
    * add error and focus border colors
    
    * fix border colors
    
    * Adding missing colors, adjusting spacing to accommodate design and adding a new example
    
    * Update visual reference
    
    * refactor and add onChange to textfield and textarea
    
    * some polish for TextArea
    
    * use render prop pattern instead of HOC
    
    * update visual diffs
    
    * update specs
    
    * name mock functions to ensure correct mapping
    
    * add type for mockName
    
    * update textfield example to have both onChange and debouncedOnChange
    
    * update ref image for basic TextArea
    
    * fix type of component
    
    * remove unecessary import
    
    * update jest types
    
    * move css includes into js, to reduce css size
    
    * clarify the example
    
    * pr feedback, simplify classes
    
    * update reference image for withDescription

--- src/components/TextArea/TextArea.test.tsx
+++ src/components/TextArea/TextArea.test.tsx
@@ -1,14 +1,14 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
 import * as React from 'react';
 import { shallow, ShallowWrapper } from 'enzyme';
-import TextField, { TextFieldProps } from '.';
+import TextArea, { TextAreaProps } from '.';
 
-describe('<TextField />', () => {
-  let component: ShallowWrapper<TextFieldProps, {}>;
+describe('<TextArea />', () => {
+  let component: ShallowWrapper<TextAreaProps, {}>;
 
   describe('with default options', () => {
     beforeEach(() => {
-      component = shallow(<TextField />).dive().dive();
+      component = shallow(<TextArea />).dive().dive();
     });
 
     it('matches its snapshot', () => {
@@ -19,10 +19,9 @@ describe('<TextField />', () => {
   describe('with all options', () => {
     beforeEach(() => {
       component = shallow(
-        <TextField
-          prefix="PREFIX"
-          suffix="SUFFIX"
-          underlined={true}
+        <TextArea
+          rows={2}
+          autoAdjustHeight={true}
           className="CLASS"
           value="VALUE"
           label="LABEL"
@@ -35,6 +34,20 @@ describe('<TextField />', () => {
           onBlur={jest.fn().mockName('onBlur')}
           onMouseEnter={jest.fn().mockName('onMouseEnter')}
           onMouseLeave={jest.fn().mockName('onMouseLeave')}
+        />,
+      ).dive().dive();
+    });
+
+    it('matches its snapshot', () => {
+      expect(component).toMatchSnapshot();
+    });
+  });
+
+  describe('with description and no error', () => {
+    beforeEach(() => {
+      component = shallow(
+        <TextArea
+          description="DESCRIPTION"
         />,
       ).dive().dive();
     });
---
+++ src/util/DebouncedOnChange/index.tsx
@@ -0,0 +1,78 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+import * as React from 'react';
+import { Async } from 'office-ui-fabric-react/lib/Utilities';
+
+export interface DebouncedOnChangeProps {
+  /**
+   * Callback for the onChange event.
+   */
+  onChange?: (newValue: any) => void;
+
+  /**
+   * Debounced callback for the onChange event.
+   */
+  debouncedOnChange?: (newValue: any) => void;
+
+  /**
+   * Component will trigger `onChange` after users stop typing for `debouncedOnChangeTime`
+   * milliseconds.
+   */
+  debouncedOnChangeTime?: number;
+}
+
+export interface DebouncedOnChangePrivateProps {
+  /**
+   * Used to pass both onChange and debouncedOnChange to the contained component.
+   */
+  unifiedOnChange?: (newValue: any) => void;
+}
+
+export interface NestedComponentProps {
+  component:
+    | React.ComponentClass<DebouncedOnChangePrivateProps>
+    | React.StatelessComponent<DebouncedOnChangePrivateProps>;
+}
+
+export default class DebouncedOnChangeComponent extends React.Component<
+  DebouncedOnChangeProps & NestedComponentProps
+> {
+  private async: Async;
+  private debouncedOnChange: (newValue: string) => void;
+
+  static defaultProps = {
+    debouncedOnChangeTime: 700,
+  };
+
+  constructor(props: DebouncedOnChangeProps) {
+    super();
+    if (props.debouncedOnChange) {
+      this.async = new Async(this);
+      this.debouncedOnChange = this.async.debounce(
+        props.debouncedOnChange,
+        props.debouncedOnChangeTime,
+      );
+    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(newValue: string) {
+    if (this.props.onChange) {
+      this.props.onChange(newValue);
+    }
+
+    if (this.props.debouncedOnChange) {
+      this.debouncedOnChange(newValue);
+    }
+  }
+
+  render() {
+    const { component: ComposedComponent } = this.props;
+    return <ComposedComponent {...this.props} unifiedOnChange={this.handleChange} />;
+  }
+
+  componentWillUnmount() {
+    if (this.async) {
+      this.async.dispose();
+    }
+  }
+}
