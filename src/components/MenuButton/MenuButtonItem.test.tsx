a454e50feecd064b9db2a67bb1413f9147e80267
Alex Henry <adhe223@g.uky.edu>
2018-02-26 17:59:21 -0800

    Typo

--- src/components/MenuButton/MenuButtonItem.test.tsx
+++ src/components/MenuButton/MenuButtonItem.test.tsx
@@ -75,7 +75,7 @@ describe('<MenuButtonItem />', () => {
           key: 'test1',
           name: 'test',
           data: {
-            yamUIuIcon: Reply,
+            yamUIIcon: Reply,
           },
         } as IContextualMenuItem,
       } as IContextualMenuItemProps;
