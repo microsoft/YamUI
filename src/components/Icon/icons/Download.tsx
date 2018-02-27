cec1405807d327177caaa2ec5027b469dd005e08
Brendan McKeon <bmckeon@yammer-inc.com>
2018-02-26 22:08:01 -0800

    Accessibility fixes for icons: prevent tabbing in IE11
    Icons need tabindex=-1 to prevent IE from tabbing to them.
    
    Also added aria-hidden="true" for good measure, to prevent any
    incidental text in any current or future SVG icons from being read
    out; we would use proper screenreader text for any text we intend
    to get read.

--- src/components/Icon/icons/Download.tsx
+++ src/components/Icon/icons/Download.tsx
@@ -8,7 +8,13 @@ import BaseIcon from '../BaseIcon';
 export default class Download extends BaseIcon {
   render() {
     return (
-      <svg viewBox="0 0 240 240" className={this.getClassName()} style={this.getStyle()}>
+      <svg
+        viewBox="0 0 240 240"
+        className={this.getClassName()}
+        style={this.getStyle()}
+        aria-hidden="true"
+        tabIndex={-1}
+      >
         <path fillRule="evenodd" d="M52.273 240v-15h135v15h-135zm140.274-114.727l-72.774 73.36L47 125.273l10.547-10.546 54.726 55.078V0h15v169.805L182 114.727l10.547 10.546z" />
       </svg>
     );
--- src/components/Icon/icons/Work.tsx
+++ src/components/Icon/icons/Work.tsx
@@ -8,7 +8,13 @@ import BaseIcon from '../BaseIcon';
 export default class Work extends BaseIcon {
   render() {
     return (
-      <svg viewBox="0 0 240 240" className={this.getClassName()} style={this.getStyle()}>
+      <svg
+        viewBox="0 0 240 240"
+        className={this.getClassName()}
+        style={this.getStyle()}
+        aria-hidden="true"
+        tabIndex={-1}
+      >
         <path fillRule="evenodd" d="M240 62v150H0V62h75V47c0-2.11.39-4.062 1.172-5.86a15.244 15.244 0 0 1 3.223-4.745 15.244 15.244 0 0 1 4.746-3.223C85.938 32.39 87.89 32 90 32h60c2.11 0 4.062.39 5.86 1.172a15.244 15.244 0 0 1 4.745 3.223 15.244 15.244 0 0 1 3.223 4.746C164.61 42.938 165 44.89 165 47v15h75zM90 62h60V47H90v15zM15 77v21.68l75 37.382V122h60v14.063l75-37.383V77H15zm90 60v15h30v-15h-30zm120 60v-81.68l-75 37.618V167H90v-14.063L15 115.32V197h210z" />
       </svg>
     );
