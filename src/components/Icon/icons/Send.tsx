87aad2fd6fea98e23e7c57bc88040adfc400af82
Adrian Mato <yensamg@gmail.com>
2018-03-02 10:49:53 -0800

    Added new Send icon (#250)
    * Added new Send icon
    
    * Moving the icon and generating the visual preview
    
    * updating the icon diff
    
    * update the unit test

---
+++ src/components/Icon/icons/Send.tsx
@@ -0,0 +1,16 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/* tslint:disable:max-line-length */
+// This file was auto-generated -- do not edit
+
+import * as React from 'react';
+import BaseIcon from '../BaseIcon';
+
+export default class Send extends BaseIcon {
+  render() {
+    return (
+      <svg viewBox="0 0 240 240" className={this.getClassName()} style={this.getStyle()}>
+        <path fillRule="evenodd" d="M1 199.672l26.484-79.336L1 41l238.008 79.336L1 199.672zm39.727-86.836h128.437L24.672 64.672l16.055 48.164zM24.672 176l144.492-48.164H40.727L24.672 176z" />
+      </svg>
+    );
+  }
+}
