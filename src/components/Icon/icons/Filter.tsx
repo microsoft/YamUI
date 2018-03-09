f54000092f40e7ee4643da3441cfd9b0da249449
Adrián Mato <yensamg@gmail.com>
2018-03-09 11:53:36 -0800

    Filter icon added to the library

---
+++ src/components/Icon/icons/Filter.tsx
@@ -0,0 +1,16 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/* tslint:disable:max-line-length */
+// This file was auto-generated -- do not edit
+
+import * as React from 'react';
+import BaseIcon from '../BaseIcon';
+
+export default class Filter extends BaseIcon {
+  render() {
+    return (
+      <svg viewBox="0 0 240 240" className={this.getClassName()} style={this.getStyle()}>
+        <path fillRule="evenodd" d="M240 15v25.664l-90 90V225H90v-94.336l-90-90V15h240zm-15 15H15v4.336l90 90V210h30v-85.664l90-90V30z" />
+      </svg>
+    );
+  }
+}
