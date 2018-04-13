273469924fbbce4b77073f69c1965ec94ed6ec37
Manuel Muñoz Solera <mamuso@mamuso.net>
2018-04-13 16:02:47 -0700

    Adding EditEvent, LiveEvent and RemoveEvent

---
+++ src/components/Icon/icons/EditEvent.tsx
@@ -0,0 +1,26 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/* tslint:disable:max-line-length */
+// This file was auto-generated -- do not edit
+
+import * as React from 'react';
+import BaseIcon from '../BaseIcon';
+
+export default class EditEvent extends BaseIcon {
+  public render() {
+    return (
+      <svg
+        viewBox="0 0 240 240"
+        className={this.getClassName()}
+        style={this.getStyle()}
+        aria-hidden="true"
+        tabIndex={-1}
+        focusable="false"
+      >
+        <path
+          fillRule="evenodd"
+          d="M240 117.924a34.404 34.404 0 0 0-6.216-3.327 34.427 34.427 0 0 0-8.784-2.309V75H15v135h104.02l-3.74 15H0V15h45V0h15v15h120V0h15v15h45v102.924zM225 60V30h-30v15h-15V30H60v15H45V30H15v30h210zm-77.406 159.406l8.71-2.172 67.4-67.588c.408-.408.695-.842.924-1.405.259-.64.372-1.212.372-1.82 0-.74-.106-1.272-.298-1.718a4.631 4.631 0 0 0-.998-1.505c-.35-.351-.756-.62-1.324-.853-.548-.225-1.177-.345-1.998-.345-.668 0-1.298.12-1.982.388a3.427 3.427 0 0 0-1.235.802l-67.395 67.49-2.176 8.726zM220.382 127c2.746 0 5.313.49 7.7 1.471 2.387.981 4.463 2.355 6.229 4.12a19.622 19.622 0 0 1 4.169 6.18c1.013 2.354 1.52 4.904 1.52 7.65 0 2.551-.49 5.036-1.471 7.456a19.039 19.039 0 0 1-4.218 6.376L163.98 230.78 127 240l9.22-36.98 70.331-70.429a18.43 18.43 0 0 1 6.376-4.169 20.263 20.263 0 0 1 7.455-1.422z"
+        />
+      </svg>
+    );
+  }
+}
