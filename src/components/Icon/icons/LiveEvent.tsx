076d0e4415246277ac93963d73091efe9f25b535
mamuso <mamuso@mamuso.net>
2018-04-16 13:01:43 -0700

    Adding EditEvent, LiveEvent and RemoveEvent (#298)
    * Adding EditEvent, LiveEvent and RemoveEvent
    
    * Remove # of icon/illustration tests

---
+++ src/components/Icon/icons/LiveEvent.tsx
@@ -0,0 +1,26 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/* tslint:disable:max-line-length */
+// This file was auto-generated -- do not edit
+
+import * as React from 'react';
+import BaseIcon from '../BaseIcon';
+
+export default class LiveEvent extends BaseIcon {
+  public render() {
+    return (
+      <svg
+        viewBox="0 0 240 240"
+        className={this.getClassName()}
+        style={this.getStyle()}
+        aria-hidden="true"
+        tabIndex={-1}
+        focusable="false"
+      >
+        <path
+          fillRule="evenodd"
+          d="M188.117 40.09C169.78 24.445 145.993 15 120 15c-25.993 0-49.78 9.445-68.117 25.09L41.79 28.985C62.794 10.92 90.122 0 120 0c29.878 0 57.206 10.92 78.21 28.985L188.117 40.09zm0 159.82l10.093 11.105C177.206 229.08 149.878 240 120 240c-29.878 0-57.206-10.92-78.21-28.985l10.093-11.105C70.22 215.555 94.007 225 120 225c25.993 0 49.78-9.445 68.117-25.09zM240 180.938l-60-30V180H0V60h180v29.063l60-30v121.874zM165 75H15v90h150V75zm60 8.32l-45 22.618v28.124l45 22.618V83.32z"
+        />
+      </svg>
+    );
+  }
+}
