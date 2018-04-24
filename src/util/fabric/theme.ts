2137fa7adfa62b8dad1d07af4c5b7003692b38f7
Chris Mohr <chrismohr@users.noreply.github.com>
2018-04-24 14:34:33 -0700

    bump office-ui-fabric-react to 5.85.0 (#300)
    * bump office-ui-fabric-react to 5.85.0
    
    * update fabric to 5.26, and updaate snaps
    
    * fix xLarge avatar initials vertical alignment
    https://github.com/OfficeDev/office-ui-fabric-react/pull/4382/files changed line-height from 70px to 72px
    
    * increase specificity of MenuButton overrides
    
    * js styles will likely be more robust than the css overrides
    
    * fix callout borders
    
    * update snaps for style overrides
    
    * define colors in one ts file
    
    * use color names that align with our existing colors.css file
    
    * px unecessary for 0
    
    * fix dropdown item alignment

--- src/util/fabric/theme.ts
+++ src/util/fabric/theme.ts
@@ -1,65 +1,42 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
 
 /**
- * Override Fabric's theme variables. Note this this window.FabricConfig is temporary,
- * soon Fabric will have a loadTheme() utility to handle this.
+ * Override Fabric's theme variables.
  */
 
-import { loadTheme, IRawStyle } from 'office-ui-fabric-react/lib/Styling';
+import { loadTheme } from 'office-ui-fabric-react/lib/Styling';
+import { theme } from '../colors';
 
-const fontStyles: IRawStyle = {
-  color: '#343A41',
-};
-
-// NOTE: Would be handy to have access to our CSS variable values in JS...
 loadTheme({
-  palette: {
-    themePrimary: '#6c98d9',
-    neutralSecondary: '#646d7a',
-    neutralTertiary: '#dde0e6',
-    neutralLighter: '#f3f5f8',
-    neutralDark: '#dde0e6',
-    redDark: '#d1423b',
-  },
+  palette: theme.palette,
   fonts: {
-    tiny: fontStyles,
     xSmall: {
-      ...fontStyles,
       fontSize: '1.0rem',
       lineHeight: '1.6rem',
     },
     small: {
-      ...fontStyles,
       fontSize: '1.2rem',
       lineHeight: '1.6rem',
     },
     smallPlus: {
-      ...fontStyles,
       fontSize: '1.4rem',
       lineHeight: '2.0rem',
     },
     medium: {
-      ...fontStyles,
       fontSize: '1.5rem',
       lineHeight: '2.0rem',
     },
-    mediumPlus: fontStyles,
     large: {
-      ...fontStyles,
       fontSize: '1.8rem',
       lineHeight: '2.4rem',
     },
     xLarge: {
-      ...fontStyles,
       fontSize: '2.2rem',
       lineHeight: '2.8rem',
     },
     xxLarge: {
-      ...fontStyles,
       fontSize: '2.4rem',
       lineHeight: '2.8rem',
     },
-    superLarge: fontStyles,
-    mega: fontStyles,
   },
 });
