25fe457b0655b1dd0b4082b3c194d18469bc1534
Chris Mohr <chrismo@microsoft.com>
2018-05-15 15:11:17 -0700

    fix tooltip

--- src/util/styles.ts
+++ src/util/styles.ts
@@ -5,7 +5,12 @@ import { ICSSPixelUnitRule } from '@uifabric/merge-styles/lib/IRawStyleBase';
 const borderRadiusSoft: ICSSPixelUnitRule = '2px';
 const fontWeightBold: IFontWeight = '600';
 
+const fontSizeSmall = '1.2rem';
+const lineHeightSmall = '1.6rem';
+
 export default {
+  fontSizeSmall,
+  lineHeightSmall,
   fontWeightBold,
   borderRadiusSoft,
 };
