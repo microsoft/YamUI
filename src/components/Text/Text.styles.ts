f757d60a19543b3b5e83a6526963b5c51c9a682b
Chris Mohr <chrismo@microsoft.com>
2018-05-24 09:05:08 -0700

    fix linting

--- src/components/Text/Text.styles.ts
+++ src/components/Text/Text.styles.ts
@@ -3,23 +3,6 @@ import { TextProps, TextSize, PrivateTextProps } from './Text';
 import { ellipsisStyle, fontWeightBold, fontSizes, lineHeights, textColors, Sizes } from '../../util/styles/fonts';
 import { IRawStyle } from '@uifabric/styling';
 
-export const getStyles = (props: TextProps & PrivateTextProps): IRawStyle => {
-  const { size, maxWidth, bold, uppercase, color, blockTextSize } = props;
-
-  return {
-    ...(maxWidth ? ellipsisStyle : {}),
-    fontWeight: bold ? fontWeightBold : undefined,
-    display: 'inline-block',
-    textTransform: uppercase ? 'uppercase' : undefined,
-    fontSize: size ? fontSizes[size] : undefined,
-    lineHeight: size ? lineHeights[size] : undefined,
-    color: color ? textColors[color] : undefined,
-    maxWidth: maxWidth || undefined,
-    height: maxWidth ? getHeight(size) : undefined,
-    verticalAlign: maxWidth ? getVerticalAlign(size || blockTextSize) : undefined,
-  };
-};
-
 const heights: Sizes = {
   xSmall: '1.6rem',
   small: '1.6rem',
@@ -46,4 +29,21 @@ const verticalAligns: Sizes = {
 
 const getVerticalAlign = (size?: TextSize) => {
   return size ? verticalAligns[size] : '-0.4rem';
+};
+
+export const getStyles = (props: TextProps & PrivateTextProps): IRawStyle => {
+  const { size, maxWidth, bold, uppercase, color, blockTextSize } = props;
+
+  return {
+    ...(maxWidth ? ellipsisStyle : {}),
+    fontWeight: bold ? fontWeightBold : undefined,
+    display: 'inline-block',
+    textTransform: uppercase ? 'uppercase' : undefined,
+    fontSize: size ? fontSizes[size] : undefined,
+    lineHeight: size ? lineHeights[size] : undefined,
+    color: color ? textColors[color] : undefined,
+    maxWidth: maxWidth || undefined,
+    height: maxWidth ? getHeight(size) : undefined,
+    verticalAlign: maxWidth ? getVerticalAlign(size || blockTextSize) : undefined,
+  };
 };
