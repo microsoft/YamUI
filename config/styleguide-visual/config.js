f6de0aaa305bdd535553db9770616d2f97aca462
Chris Mohr <chrismo@microsoft.com>
2018-05-25 11:26:17 -0700

    update rsv to 0.6.3

--- config/styleguide-visual/config.js
+++ config/styleguide-visual/config.js
@@ -13,7 +13,7 @@ module.exports = {
   },
   launchOptions: {
     args: ['--no-sandbox', '--disable-setuid-sandbox', '--font-render-hinting medium'],
-    slowMo: 30,
+    slowMo: 40,
   },
   navigationOptions: {
     waitUntil: 'networkidle0',
