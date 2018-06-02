0096e066439698097b5cf21e313a5bf031451022
Andrew Brassington <jabrassi@microsoft.com>
2018-06-01 19:10:37 -0700

    WIP: SuggestionsListItem and Basepicker

--- src/components/SuggestionsList/index.ts
+++ src/components/SuggestionsList/index.ts
@@ -1,3 +1,5 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
 export { default } from './SuggestionsList';
 export * from './SuggestionsList';
+export { default as SuggestionsListItem } from './SuggestionsListItem';
+export * from './SuggestionsListItem';
