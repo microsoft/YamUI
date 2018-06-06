e42522b6db59b92dce6b3aa856fcc529d3f5eceb
Chris Mohr <chrismo@microsoft.com>
2018-06-06 09:55:46 -0700

    add style file

---
+++ src/components/Avatar/Avatar.styles.ts
@@ -0,0 +1,48 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+import { palette } from '../../util/colors';
+import { mergeStyleSets } from '@uifabric/styling';
+import { memoizeFunction } from '@uifabric/utilities';
+import { IPersonaCoinStyleProps, IPersonaCoinStyles } from 'office-ui-fabric-react/lib/PersonaCoin';
+import { AvatarSize, BorderType } from './Avatar';
+import { borders } from '../../util/styles/borders';
+
+export interface AvatarStyleProps {
+  borderType: BorderType;
+  size: AvatarSize;
+}
+
+export const getPersonaCoinGetStylesFunction = (
+  props: AvatarStyleProps,
+): ((styleProps: IPersonaCoinStyleProps) => Partial<IPersonaCoinStyles>) => {
+  const { borderType, size } = props;
+  const borderRadius = borders[borderType];
+  const initials = size === AvatarSize.XLARGE ? { lineHeight: '70px' } : {};
+  return () => ({
+    initials,
+    image: {
+      borderRadius,
+    },
+    imageArea: {
+      borderRadius,
+    },
+  });
+};
+
+export const getClassNames = memoizeFunction((size: AvatarSize) =>
+  mergeStyleSets({
+    root: {
+      position: 'relative',
+    },
+    badge: {
+      position: 'absolute',
+      height: size === AvatarSize.SMALL || size === AvatarSize.XSMALL ? '16px' : '20px',
+      width: size === AvatarSize.SMALL || size === AvatarSize.XSMALL ? '16px' : '20px',
+      bottom: '-2px',
+      right: '-2px',
+      borderRadius: borders.round,
+      border: `solid 2px ${palette.white}`,
+      background: palette.white,
+      color: palette.themeDark,
+    },
+  }),
+);
