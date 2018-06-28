24335c25e6b04969721f5c664c898894ec7fa032
Chris Mohr <chrismo@microsoft.com>
2018-06-28 16:41:15 -0700

    add missing import

--- src/components/Avatar/Avatar.test.tsx
+++ src/components/Avatar/Avatar.test.tsx
@@ -1,6 +1,6 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
 import * as React from 'react';
-import { shallow, mount, ShallowWrapper } from 'enzyme';
+import { shallow, mount, ShallowWrapper, ReactWrapper } from 'enzyme';
 import Text from '../Text';
 import CustomizableAvatar, { Avatar } from './Avatar';
 import { AvatarProps, AvatarSize, BorderType } from './Avatar.types';
