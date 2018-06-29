7af2ec14cb274890ced51a00014d68b64af28819
Chris Mohr <chrismo@microsoft.com>
2018-06-29 14:38:13 -0700

    customizable PreviewCard

--- src/components/PreviewCard/PreviewCard.styles.ts
+++ src/components/PreviewCard/PreviewCard.styles.ts
@@ -1,14 +1,13 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
-import { getTheme } from '../../util/colors';
-import { mergeStyleSets } from '@uifabric/styling';
+import { mergeStyleSets, ITheme } from '@uifabric/styling';
 import { memoizeFunction } from '@uifabric/utilities';
 
 export interface PreviewCardStyleProps {
   allowOnClick: boolean;
 }
 
-const getMemoizedClassNames = memoizeFunction((styleProps, theme) => {
-  const { allowOnClick } = styleProps;
+export const getClassNames = memoizeFunction(styleProps => {
+  const { allowOnClick, theme } = styleProps;
 
   return mergeStyleSets({
     root: {
@@ -35,8 +34,3 @@ const getMemoizedClassNames = memoizeFunction((styleProps, theme) => {
     },
   });
 });
-
-export const getClassNames = (styleProps: PreviewCardStyleProps) => {
-  const theme = getTheme();
-  return getMemoizedClassNames(styleProps, theme);
-};
