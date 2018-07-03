46e83dd617956a9ecdc8bad10a57bcf6e8542121
Chris Mohr <chrismohr@users.noreply.github.com>
2018-07-03 14:27:19 -0700

    Customizer background color (#442)
    * customizable background color within Customizable
    
    * allow className prop
    
    * also body text color
    
    * update examples, and add font to Customizer
    
    * update snapshots for font

--- src/components/Customizer/Customizer.tsx
+++ src/components/Customizer/Customizer.tsx
@@ -1,13 +1,21 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
 import '../../yamui';
 import * as React from 'react';
+import { NestableBaseComponentProps } from '../../util/BaseComponent/props';
 import { Customizer as FabricCustomizer, ICustomizerProps } from 'office-ui-fabric-react/lib/Utilities';
+import CustomizableBody from './CustomizableBody';
 
 /**
  * Used to inject a custom theme object into customizable components, overriding the global theme.
  */
-export default class Customizer extends React.Component<ICustomizerProps> {
+export default class Customizer extends React.Component<ICustomizerProps & NestableBaseComponentProps> {
   public render() {
-    return <FabricCustomizer {...this.props} />;
+    const { children, className, ...customizerProps } = this.props;
+
+    return (
+      <FabricCustomizer {...customizerProps}>
+        <CustomizableBody {...{ children, className }} />
+      </FabricCustomizer>
+    );
   }
 }
