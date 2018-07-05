dbb447cd8156dd7b6bb03a9e5509dd3a08f93607
Daniel Perez Alvarez <unindented@gmail.com>
2018-07-05 12:46:44 -0700

    Avoid importing the root `office-ui-fabric-react`
    It will cause consumers to pull all of Fabric.

--- src/components/Customizer/Customizer.types.ts
+++ src/components/Customizer/Customizer.types.ts
@@ -1,6 +1,6 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
 import { ICustomizerProps, customizable } from 'office-ui-fabric-react/lib/Utilities';
-import { createTheme, ITheme } from 'office-ui-fabric-react';
+import { createTheme, ITheme } from 'office-ui-fabric-react/lib/Styling';
 import { theme as defaultTheme } from '../../util/theme';
 
 export { createTheme, ITheme, ICustomizerProps, customizable, defaultTheme };
