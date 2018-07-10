dc8607ca680c01ef63864fdef07f9d9b61486393
Edward Drost <eddrosttoo@gmail.com>
2018-07-10 16:03:00 -0700

    Addition(YamUI) Adding praise icons into assets/illustrations.
    Ran svg task and built the 72px TSX files.

---
+++ src/components/Illustration/illustrations/PraiseCheckeredFlagHC72.tsx
@@ -0,0 +1,30 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/* tslint:disable:max-line-length */
+// This file was auto-generated -- do not edit
+
+import * as React from 'react';
+import BaseIllustration from '../BaseIllustration';
+
+export default class PraiseCheckeredFlagHC extends BaseIllustration {
+  public render() {
+    const { size = 72 } = this.props;
+
+    return (
+      <svg
+        viewBox="0 0 72 72"
+        className={this.getClassName(size)}
+        aria-hidden="true"
+        tabIndex={-1}
+        focusable="false"
+      >
+        <ellipse cx="-34.896" cy="37.08" rx="35.999" ry="35.992" transform="rotate(-88.267) skewX(-.001)" />
+  <path fill="#fff" d="M36.012 0v1.104h1.056c9.31.288 17.949 4.176 24.332 10.944 6.382 6.768 9.742 15.648 9.454 24.96-.288 9.12-4.031 17.664-10.606 24-6.527 6.384-15.118 9.888-24.236 9.888h-1.056c-9.31-.288-17.949-4.176-24.332-10.944-6.43-6.816-9.79-15.696-9.502-25.008.288-9.12 4.031-17.664 10.606-24 6.575-6.336 15.165-9.84 24.284-9.84V0m0 0C16.623 0 .642 15.408.018 34.896c-.624 19.872 15.021 36.48 34.89 37.056h1.104c19.389 0 35.37-15.408 35.946-34.896C72.582 17.184 56.936.576 37.068 0z" />
+  <path fill="#fff" d="M20.079 17.328h13.245v9.312H20.08zm39.257 9.312H46.09v-9.312h9.407a3.851 3.851 0 0 1 3.839 3.84z" />
+  <path fill="none" d="M20.27 26.496h13.246v9.312H20.271z" />
+  <path fill="#fff" d="M33.276 26.496h13.246v9.312H33.276zM20.08 35.808h13.245v9.312H20.08zm35.466 9.312H46.09v-9.312h13.246v5.52a3.77 3.77 0 0 1-3.791 3.792z" />
+  <path fill="#fff" d="M19.55 62.64a1.823 1.823 0 0 1-1.823-1.824V16.704a1.823 1.823 0 1 1 3.647 0v44.112c.048 1.008-.815 1.824-1.823 1.824z" />
+  <path fill="#fff" d="M50.985 18.816l6.863 5.28v13.68l-7.15 5.904H21.71V18.816h29.275m.48-1.488H20.271V45.12h30.954l8.11-6.672V23.376z" />
+      </svg>
+    );
+  }
+}
---
+++ src/components/Illustration/illustrations/PraisePieHC72.tsx
@@ -0,0 +1,31 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/* tslint:disable:max-line-length */
+// This file was auto-generated -- do not edit
+
+import * as React from 'react';
+import BaseIllustration from '../BaseIllustration';
+
+export default class PraisePieHC extends BaseIllustration {
+  public render() {
+    const { size = 72 } = this.props;
+
+    return (
+      <svg
+        viewBox="0 0 72 72"
+        className={this.getClassName(size)}
+        aria-hidden="true"
+        tabIndex={-1}
+        focusable="false"
+      >
+        <circle cx="-34.889" cy="37.078" r="35.994" transform="rotate(-88.267)" />
+  <path fill="#fff" d="M36.014 0v1.104h1.055c9.311.288 17.95 4.175 24.333 10.943 6.384 6.767 9.743 15.646 9.455 24.956C70.57 46.123 66.826 54.665 60.25 61c-6.527 6.384-15.118 9.887-24.236 9.887h-1.056c-9.311-.288-17.95-4.175-24.333-10.942C4.194 53.129.834 44.25 1.122 34.94c.288-9.12 4.031-17.662 10.607-23.997 6.575-6.335 15.166-9.839 24.285-9.839V0m0 0C16.624 0 .642 15.406.018 34.892-.606 54.762 15.04 71.367 34.91 71.943h1.104c19.39 0 35.371-15.406 35.947-34.892C72.585 17.182 56.939.576 37.069.001z" />
+  <g fill="#fff" transform="matrix(.47994 0 0 .47994 .018 0)">
+    <path d="M76.5 42.1h-1.1c-2.1 0-3.9 1.8-3.9 3.9l.2 20.5c0 2.1 1.8 3.9 3.9 3.9h1.1c2.1 0 3.9-1.8 3.9-3.9L80.5 46c-.1-2.2-1.8-3.9-4-3.9zm5.4-8.3c0 3.4-2.7 6.1-6.1 6.2-3.4 0-6.1-2.7-6.2-6.1 0-3.4 6-14.2 6-14.2s6.2 10.7 6.3 14.1z" />
+    <ellipse cx="76.2" cy="80.9" rx="34.6" ry={19} />
+    <path d="M102.4 121.2H50.2l-8.7-39.3h69.6z" />
+  </g>
+  <path d="M19.312 38.635h37.195v1.344H19.312z" />
+      </svg>
+    );
+  }
+}
