13afbf6ec52453c940c1be493482d34bc5df3a1a
swese44 <swese44@gmail.com>
2018-09-05 11:14:49 -0700

    chore(styles): Scope global box-sizing rules to AppContainer and Layer components

--- src/components/AppContainer/AppContainer.styles.ts
+++ src/components/AppContainer/AppContainer.styles.ts
@@ -1,24 +1,39 @@
 /*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
-import { mergeStyleSets } from 'office-ui-fabric-react/lib/Styling';
+import { mergeStyleSets, IStyle } from 'office-ui-fabric-react/lib/Styling';
 import { memoizeFunction } from 'office-ui-fabric-react/lib/Utilities';
 
+const boxSizingStyles: IStyle = {
+  boxSizing: 'border-box',
+  selectors: {
+    '*, *::before, *::after': {
+      boxSizing: 'inherit',
+    },
+  },
+};
+
 export const getClassNames = memoizeFunction(() => {
-  return mergeStyleSets({
-    root: {
-      selectors: {
-        ':global(html)': {
-          /* Sets 1.0rem to 10px */
-          fontSize: '62.5%',
-          boxSizing: 'border-box',
-        },
-        ':global(html, body)': {
-          margin: 0,
-          padding: 0,
-        },
-        ':global(*, *::before, *::after)': {
-          boxSizing: 'inherit',
+  return mergeStyleSets(
+    {
+      root: boxSizingStyles,
+    },
+    {
+      root: {
+        selectors: {
+          ':global(html)': {
+            /* Sets 1.0rem to 10px */
+            fontSize: '62.5%',
+          },
+          ':global(html, body)': {
+            margin: 0,
+            padding: 0,
+          },
         },
       },
     },
-  });
+  );
 });
+
+export const scopedSettings = {
+  CalloutContent: { styles: { calloutMain: boxSizingStyles } },
+  Modal: { styles: { root: boxSizingStyles } },
+};
