8bc5d77806852cbf26c8191934c9585bf0a4b125
Hanna Koh <hakoh@microsoft.com>
2018-09-05 10:49:08 -0700

    fix(AppContainer): scope box sizing styling to AppContainer instead of global

--- src/components/AppContainer/AppContainer.styles.ts
+++ src/components/AppContainer/AppContainer.styles.ts
@@ -5,18 +5,18 @@ import { memoizeFunction } from 'office-ui-fabric-react/lib/Utilities';
 export const getClassNames = memoizeFunction(() => {
   return mergeStyleSets({
     root: {
+      boxSizing: 'border-box',
       selectors: {
+        '*, *::before, *::after': {
+          boxSizing: 'inherit',
+        },
         ':global(html)': {
           /* Sets 1.0rem to 10px */
           fontSize: '62.5%',
-          boxSizing: 'border-box',
         },
         ':global(html, body)': {
           margin: 0,
           padding: 0,
-        },
-        ':global(*, *::before, *::after)': {
-          boxSizing: 'inherit',
         },
       },
     },
