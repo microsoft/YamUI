820d6ffd4f346c02ef4d65278caf86c0dee62709
Tony Jackson <tjackson@yammer-inc.com>
2018-11-06 12:38:56 -0800

    feat(QuestionMessage icon): Add new Q & A msg icon - NOTE: The QuestionMessage.svg is not exactly the right format so we will need a new one from design before we complete this PR - NOTE: We need to resolve the best process for releasing the icon to both master and potentially a minor branch of of 8.16.1 in order to consume it from projects which depend on baseline/reset styles that were removed - NOTE: We may also choose to revisit this PR and resurrect normalize.css and our base element resets which were removed: https://github.com/Microsoft/YamUI/pull/507

---
+++ src/components/Icon/icons/QuestionMessage.tsx
@@ -0,0 +1,24 @@
+/*! Copyright (c) Microsoft Corporation. All rights reserved. Licensed under the MIT license. */
+/* tslint:disable:max-line-length */
+// This file was auto-generated -- do not edit
+
+import * as React from 'react';
+import BaseIcon from '../BaseIcon';
+
+export default class QuestionMessage extends BaseIcon {
+  public render() {
+    return (
+      <svg
+        viewBox="0 0 240 240"
+        className={this.getClassName()}
+        style={this.getStyle()}
+        aria-hidden="true"
+        tabIndex={-1}
+        focusable="false"
+      >
+        <path d="M11 1.1v1h2.2v7.8H4.5l-2 2v-2H1V2.1h2.1v-1H0v9.7h1.5l.1 3.3L5 10.8h9.2V1.1z" className="st0" />
+  <path d="M5.4 2.6C5.6 1.6 6.1 1 7 1c.5 0 .8.1 1.1.4.3.3.4.6.4 1.1 0 .2-.1.4-.2.6-.1.3-.3.6-.8.9-.5.4-.8.8-1 1.1-.1.4-.2 1-.2 1.7v.1h1.2v-.1c0-.5.1-.9.2-1.2.1-.2.4-.5.9-.9s.8-.8 1-1.1c.2-.3.3-.7.3-1.1 0-.7-.3-1.3-.8-1.8C8.5.2 7.8 0 7 0S5.5.2 5 .7c-.6.5-.9 1.1-1 1.9v.1l1.4-.1zM6.2 7.6h1.4V9H6.2z" className="st0" />
+      </svg>
+    );
+  }
+}
