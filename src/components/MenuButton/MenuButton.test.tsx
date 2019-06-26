5abcee8be27d73609d32b8aa75ffb63e15589bcb
Alex Waibel <51180308+alexwaibelmsft@users.noreply.github.com>
2019-06-26 16:37:53 -0700

    Use double quote instead of JSX for string literal

--- src/components/MenuButton/MenuButton.test.tsx
+++ src/components/MenuButton/MenuButton.test.tsx
@@ -135,7 +135,7 @@ describe('<MenuButton />', () => {
           menuItems={menuItems}
           className="TEST-ADDITIONAL-CLASSNAME"
           icon={Reply}
-          text={'TEST'}
+          text="TEST"
         />,
       );
     });
